025b53c1d95ac0376b4f54ff28408b133ab418d3
grudus <jakgruda@gmail.com>
2018-12-08 22:46:08 +0100

    Solve 2nd star of 7th day

--- src/com/grudus/adventofcode/day07/TheSumOfItsParts.kt
+++ src/com/grudus/adventofcode/day07/TheSumOfItsParts.kt
@@ -1,6 +1,7 @@
 package com.grudus.adventofcode.day07
 
 import com.grudus.adventofcode.day07.TheSumOfItsParts.firstStar
+import com.grudus.adventofcode.day07.TheSumOfItsParts.secondStar
 import com.grudus.adventofcode.findGroups
 import com.grudus.adventofcode.readDayInput
 
@@ -15,12 +16,21 @@ object TheSumOfItsParts {
         private val children = mutableListOf<Node<T>>()
 
         private var visited = isVisited
+        private var taken = false
+
+        override fun equals(other: Any?) = (other as Node<T>).value == value
 
         override fun toString() = value.toString()
 
         fun markAsVisited() {
             visited = true
         }
+
+        fun take() {
+            taken = true
+        }
+
+        fun isFree() = !taken
 
         fun addChild(node: Node<T>) {
             children += node
@@ -38,9 +48,9 @@ object TheSumOfItsParts {
             else children.all { it.allVisited() }
 
 
-        fun visit(toVisit: List<Node<T>> = emptyList()): List<Node<T>> =
+        fun visit(toVisit: Set<Node<T>> = emptySet()): Set<Node<T>> =
             if (visited) {
-                children.flatMap { it.visit(toVisit) }
+                children.flatMap { it.visit(toVisit) }.toSet()
             } else {
                 if (parents.all { it.visited })
                     toVisit + this
@@ -60,6 +70,57 @@ object TheSumOfItsParts {
                 val nodeToVisit = notVisitedNodes.minBy { it.value }!!
                 nodeToVisit.markAsVisited()
                 order + nodeToVisit.value
+            }
+    }
+
+    class Worker<T> {
+        var currentWork: T? = null
+        private var endTime = Int.MAX_VALUE
+
+        fun setWork(work: T, endTime: Int) {
+            this.currentWork = work
+            this.endTime = endTime;
+        }
+
+        fun cleanWork() {
+            currentWork = null;
+            endTime = Int.MAX_VALUE;
+        }
+
+        fun isWorking() = currentWork != null
+        fun isSiesta(time: Int) = endTime <= time
+    }
+
+    fun secondStar(input: List<String>): Int {
+        val tree = createTree(input)
+        val numberOfWorkers = 5
+        val baseTime = 60
+        val workers = (0 until numberOfWorkers).map { Worker<Node<String>>() }
+
+        return generateSequence({ 0 }, { it + 1 })
+            .takeWhile { !tree.allVisited()}
+            .fold(1) { _, second ->
+                val availableWorkers = workers.filter { !it.isWorking() }
+
+                val notVisitedNodes: List<Node<String>> = tree.visit().filter { it.isFree() }
+
+                val nodesToVisit = notVisitedNodes.sortedBy { it.value }.take(availableWorkers.size)
+
+                availableWorkers.take(nodesToVisit.size).forEachIndexed { i, worker ->
+                    val workTime = nodesToVisit[i].value[0].toInt() - 65 + baseTime
+                    nodesToVisit[i].take()
+                    worker.setWork(nodesToVisit[i],
+                    second + workTime)
+                }
+
+                workers.filter { it.isSiesta(second) }
+                    .forEach {
+                        val work = it.currentWork
+                        work!!.markAsVisited()
+                        it.cleanWork()
+                    }
+
+                second + 1
             }
     }
 
@@ -103,5 +164,6 @@ fun main(args: Array<String>) {
     val input = readDayInput("07")
 
     println(firstStar(input))
+    println(secondStar(input))
 
 }
