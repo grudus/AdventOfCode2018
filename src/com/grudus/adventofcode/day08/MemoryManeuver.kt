59abb34b33c6c21e6da04b79c9f8fd93fd5aff4b
grudus <jakgruda@gmail.com>
2018-12-09 19:16:50 +0100

    Second part of the 8th day

--- src/com/grudus/adventofcode/day08/MemoryManeuver.kt
+++ src/com/grudus/adventofcode/day08/MemoryManeuver.kt
@@ -5,39 +5,61 @@ import java.util.*
 
 object MemoryManeuver {
 
-    data class Node(var childCount: Int, var metadataCount: Int? = null)
+    data class Node(var childCount: Int, var metadataCount: Int? = null) {
+        val children = mutableListOf<Node>()
+        val metadata = mutableListOf<Int>()
 
-    fun firstStar(input: List<Int>): Int {
-        val stack = ArrayDeque<Node>().apply { push(Node(input[0], input[1])) }
+        override fun toString() = metadata.toString()
 
-        return input.drop(2).fold(0) { metaSum, num ->
+        fun sumMeta(): Int =
+            metadata.sum() + children.map { it.sumMeta() }.sum()
+
+        fun sumMeta2(): Int {
+            if (children.isEmpty())
+                return metadata.sum()
+            return metadata.map { it - 1 }.map { metaIndex ->
+                if (metaIndex >= children.size)
+                    0
+                else
+                    children[metaIndex].sumMeta2()
+            }.sum()
+        }
+    }
+
+    fun firstStar(input: List<Int>): Int =
+        createTree(input)
+            .sumMeta()
+
+
+    fun secondStar(input: List<Int>): Int =
+        createTree(input).sumMeta2()
+
+    
+    private fun createTree(input: List<Int>): Node {
+        val root = Node(input[0], input[1])
+        val stack = ArrayDeque<Node>().apply { push(root) }
+
+        input.drop(2).forEach { num ->
             val current = stack.pop()
 
             if (current.metadataCount == null) {
                 current.metadataCount = num
                 stack.push(current)
-                return@fold metaSum
-            }
-
-            if (current.childCount > 0) {
+            } else if (current.childCount > 0) {
                 current.childCount--
                 stack.push(current)
-                stack.push(Node(num))
-                return@fold metaSum
-            }
-
-            if (current.childCount == 0) {
+                val node = Node(num)
+                stack.push(node)
+                current.children += node
+            } else if (current.childCount == 0) {
                 current.metadataCount = current.metadataCount!! - 1
+                current.metadata += num
                 if (current.metadataCount!! > 0)
                     stack.push(current)
-
-                return@fold metaSum + num
             }
-
-            metaSum
         }
+        return root
     }
-
 }
 
 
@@ -47,4 +69,5 @@ fun main(args: Array<String>) {
         .map { it.toInt() }
 
     println(MemoryManeuver.firstStar(input))
+    println(MemoryManeuver.secondStar(input))
 }
