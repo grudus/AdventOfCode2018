e3e2ddf256b8008db27689c11cbb8734f61a21d1
grudus <jakgruda@gmail.com>
2018-12-14 17:31:02 +0100

    Remove solution based on custom circular linked list

--- src/com/grudus/adventofcode/day14/ChocolateCharts.kt
+++ src/com/grudus/adventofcode/day14/ChocolateCharts.kt
@@ -9,10 +9,7 @@ object ChocolateCharts {
         var elf2Index = 1
 
         while (recipies.size < numberOfRecipies + 10) {
-            val newRecipies = (recipies[elf1Index] + recipies[elf2Index]).toString()
-                .map { Character.getNumericValue(it) }
-
-            recipies += newRecipies
+            recipies += (recipies[elf1Index] + recipies[elf2Index]).toString().map(Character::getNumericValue)
 
             elf1Index = (elf1Index + recipies[elf1Index] + 1) % recipies.size
             elf2Index = (elf2Index + recipies[elf2Index] + 1) % recipies.size
@@ -22,102 +19,25 @@ object ChocolateCharts {
     }
 
 
-    class Node(val value: Int) {
-        var next: Node? = null
-        var previous: Node? = null
+    fun secondStar(searchedSubsequence: String): Int {
+        val recipies = mutableListOf(3, 7)
 
-        init {
-            ++size
+        var elf1Index = 0
+        var elf2Index = 1
+
+        while (searchedSubsequence !in recipies.takeLast(11).joinToString("")) {
+            recipies += (recipies[elf1Index] + recipies[elf2Index]).toString().map(Character::getNumericValue)
+
+            elf1Index = (elf1Index + recipies[elf1Index] + 1) % recipies.size
+            elf2Index = (elf2Index + recipies[elf2Index] + 1) % recipies.size
         }
 
-        private fun addNext(value: Int): Node {
-            val newNode = Node(value)
-            if (this.next == null) {
-                this.next = newNode
-                newNode.previous = this
-            } else {
-                val previousNext = this.next
-
-                this.next = newNode
-                newNode.previous = this
-
-                newNode.next = previousNext
-                previousNext!!.previous = newNode
-            }
-            return this.next!!
-        }
-
-
-        companion object {
-            var size = 0
-            lateinit var last: Node
-
-            fun addLast(value: Int) {
-                last = last.addNext(value)
-            }
-
-            fun createCircular(value1: Int, value2: Int): Node {
-                val node: Node = Node(value1).addNext(value2)
-                node.next = node.previous
-                node.previous!!.previous = node
-                last = node
-                return node
-            }
-
-            fun checkIfLastMatches(sequence: List<Int>): Boolean {
-                var last = Node.last
-
-                return sequence.reversed().all {
-                    val value = last.value
-                    last = last.previous!!
-                    it == value
-                }
-            }
-        }
+        return recipies.joinToString("").indexOf(searchedSubsequence)
     }
-
-
-    fun secondStar(input: String): Int {
-        val recipies = Node.createCircular(3, 7)
-        val sequence = input.map { Character.getNumericValue(it) }
-
-        var elf1 = recipies
-        var elf2 = recipies.next!!
-
-        while (true) {
-            val newRecipies = (elf1.value + elf2.value).toString()
-                .map { Character.getNumericValue(it) }
-
-            Node.addLast(newRecipies[0])
-
-            if (Node.checkIfLastMatches(sequence))
-                return Node.size - sequence.size
-
-            if (newRecipies.size == 2) {
-                Node.addLast(newRecipies[1])
-
-                if (Node.checkIfLastMatches(sequence))
-                    return Node.size - sequence.size
-            }
-
-            (0..elf1.value).forEach {
-                elf1 = elf1.next!!
-            }
-
-            (0..elf2.value).forEach {
-                elf2 = elf2.next!!
-            }
-        }
-
-    }
-
-
 }
 
 
 fun main(args: Array<String>) {
-    val numberOfRecipies = 30121
-
-    println(ChocolateCharts.firstStar(numberOfRecipies))
+    println(ChocolateCharts.firstStar(30121))
     println(ChocolateCharts.secondStar("030121"))
 }
